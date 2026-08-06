770d33e73fe49b91b7861bf8fa30f4cb1fdfd0be
ShreyashDevali2518 <mscds.2518@unigoa.ac.in>
2026-08-06 16:01:36 +0530

    added registration page

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder="../templates",
+            static_folder="../static")
 
 @app.route("/")
 def home():
@@ -9,6 +11,9 @@ def home():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+# Required for Vercel
+app = app
 
 if __name__ == "__main__":
     app.run(debug=True)
